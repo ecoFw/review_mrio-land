0dcb4359a2bb7bb047f452cdbd4d9fd261a44ec0
MKLau <mk@mklau.info>
2020-04-28 11:46:59 -0400

    Updates travis

--- make.sh
+++ make.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
 git config --global user.email "mk@mklau.info"
 git config --global user.name "mklau"
 
-git clone https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git 
-cd ${TRAVIS_REPO_SLUG}
+git clone https://github.com/ecoFw/review_mrio-land.git 
+cd review_mrio-land
 Rscript make.R
 git add -A .
-git commit -am "Make project" 
+git commit -am "Travis make project" 
 git push -q origin master
